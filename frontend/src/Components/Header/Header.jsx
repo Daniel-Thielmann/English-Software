7135cefef425443f51e24134f50e745556a9d549
Daniel Thielmann <daniel.thielmann@example.com>
2025-03-12 10:27:55 -0300

    simple <p> update

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -12,13 +12,25 @@ const Header = ({ comecarPratica }) => {
         <h1>
           A <span>nova maneira</span> de aprender inglês
         </h1>
-        <p>Aprimore seu entendimento em apenas alguns minutos, diariamente!</p>
         <p>
-          No Listening & Writing, você ouvirá frases em inglês e precisará
-          escrevê-las corretamente para aprimorar sua compreensão auditiva e
-          ortografia. Já no Listening & Speaking, você praticará a pronúncia ao
-          repetir frases, sendo avaliado por IA para melhorar sua fluência na
-          fala.
+          Aprimore seu entendimento em apenas alguns minutos! Com auxilio
+          completo da melhor tecnologia de Inteligência Artificial
+        </p>
+        <p>
+          No Listening & Writing, você ouvirá frases em inglês geradas por IA e
+          precisará escrevê-las corretamente para aprimorar sua compreensão
+          auditiva e ortografia.
+          <br></br>
+          <br></br>
+          Já no Listening & Speaking, você praticará a pronúncia ao repetir
+          frases geradas por IA, sendo avaliado por IA para melhorar sua
+          fluência na fala.
+          <br></br>
+          <br></br>
+          Com a Conversação IA, você poderá conversar livremente com a
+          inteligência artificial, praticando a comunicação em tempo real. Fale
+          por 30 minutos e ganhe 100 pontos para aprimorar ainda mais suas
+          habilidades no idioma e subir no Ranking!
         </p>
       </div>
     </header>
